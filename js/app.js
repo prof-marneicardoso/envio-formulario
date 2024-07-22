d1cb4b653fd543d32aa0dc1e27b3906c84604f96
Marnei <marneicardoso.geracaotech@gmail.com>
2024-07-22 12:28:51 -0300

    Update 1

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 
-
 const formulario = document.getElementById('formulario');
-const mensagem = document.getElementById('mensagem');
+const statusEnvio = document.getElementById('statusEnvio');
 
 // console.log(formulario); // LOG
 
@@ -17,8 +16,9 @@ formulario.addEventListener('submit', function (evento) {
     const dados = Object.fromEntries(formData);
     // console.log(dados);  // LOG
 
-    mensagem.innerHTML = "Por favor, aguarde..."
+    statusEnvio.innerHTML = "Por favor, aguarde..."
 
+    /*
     // Envia os dados do formulário para a API
     fetch('https://api.web3forms.com/submit', {
         method: 'POST',
@@ -49,31 +49,5 @@ formulario.addEventListener('submit', function (evento) {
             mensagem.innerHTML = 'Ops, algo deu errado! Verifique o e-mail informado';
         }
     })
-});
-
-
-
-/*
-const mensagem = document.querySelector('#mensagem');
-const formulario = document.querySelector("#formulario");
-
-formulario.addEventListener("submit", formSubmit);
-
-function formSubmit(evento) {
-    evento.preventDefault();
-
-    const formData = new FormData(evento.target);
-
-    fetch("https://getform.io/f/anlenqda", {
-        method: "POST",
-        body: formData,
-        headers: {
-            "Accept": "application/json",
-        },
-    })
-    .then(response => {
-        console.log(response);
-    })
-    .catch(error => console.log(error));
-}
     */
+});
